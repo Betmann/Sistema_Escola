82349a9d51e91daf41407715c615f80c96ce8292
Betmann <saldanabetman@gmail.com>
2024-05-18 14:09:41 -0300

    18-05-2024

---
+++ Py/main.py
@@ -0,0 +1,117 @@
+# importa o modulo http.server
+import os
+from http.server import SimpleHTTPRequestHandler
+import socketserver
+from urllib.parse import parse_qs
+
+class MyHandley(SimpleHTTPRequestHandler):
+    def list_directory(self, path):
+        try:
+            # tenta abrir o arquivo
+            f = open(os.path.join(path, "login.html"), "r")
+            # se existir, envia o conteudo do arquivo
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(f.read().encode("utf-8"))
+            f.close()
+            return None
+        except FileNotFoundError:
+            pass
+        return super().list_directory(path)
+
+    def do_GET(self):
+        if self.path == "/login":
+            try:
+                with open(os.path.join(os.getcwd(), "index.html"), "r") as login_file:
+                    content = login_file.read()
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(content.encode("utf-8"))
+            except FileNotFoundError:
+                self.send_error(404, "File not found")
+
+        elif self.path == '/login_failed':
+
+            self.send_response(200) 
+            self.send_header("Content-type", "text/html; charset=8")
+            self.end_headers()
+            # le o conteudo da pagina login.html
+            with open(os.path.join(os.getcwd(), 'login.html'), 'r', encoding='utf-8') as login_file:
+                content = login_file.read()
+            mensagem = "Login e/ou senha incorreta... tenta novamente." 
+            content = content.replace('<!-- Mensagem de erro sera inserida aqui -->',
+                                       f'<div class="error-message">{mensagem}</div>')
+            self.wfile.write(content.encode('utf-8')) 
+            
+        else:
+             super().do_GET()
+
+    def usuario_existente( self, login, senha):
+        #verificar se o login já existe no arquivo
+        with open('dados.txt', 'r', encoding='utf-8') as file:
+            for line in file:
+                stored_login, stored_senha = line.strip().split(';')
+                if login == stored_login:
+                    print("Cheguei aqui significando que localizei o login informando")
+                    print("Senha:" + senha)
+                    print("senha_armazenada:" + senha )
+                    return senha == stored_senha
+        return False      
+    
+    def do_POST(self):
+        if self.path == "/enviar_login":
+            content_length = int(self.headers["Content-Length"])
+            body = self.rfile.read(content_length).decode("utf-8")
+
+            # parseia os dados ao formulario
+            form_data = parse_qs(body, keep_blank_values=True)
+
+            # exibe os dados no terminal
+            print("Dados do formulario:")
+            print("Email:", form_data.get("email", [""])[0])
+            print("Senha:", form_data.get("senha", [""])[0])
+
+            #verificar se o usuario ja existe
+            login = form_data.get('email', [''])[0]
+            senha = form_data.get('senha', [''])[0]
+
+            if self.usuario_existente(login, senha):
+                #responde ao cliente indicando que o usuario ja consta nos registro
+                self.send_response(200)
+                self.send_header("Content-type", "text/html; charset=utf-8")
+                self.end_headers()
+                mensagem = f"Usuário {login} logado com sucesso!!!"
+                self.wfile.write(mensagem.encode("utf-8"))
+
+            else:
+                if any(line.startswith(f"{login};") for line in 
+                       open('dados.txt', 'r', encoding='utf-8')):
+                    self.send_response(302)
+                    self.send_header('Location' , '/login_falied')
+                    self.end_headers()
+                    return
+                else:
+                    with open('dados.txt', 'a', encoding='utf-8') as file:
+                        file.write(f"{login};{senha}\n")
+                        self.send_response(200)
+                        self.send_header("Content-type", "text/html; charset=utf-8")
+                        self.end_headers()
+                        mensagem= f"Hola {login}, seja bem vindo!! Percebemos que voce é novo por aqui.."
+                        self.wfile.write(mensagem.encode('utf-8'))
+        else:
+            # se nao for a rota "/submit_login", continua como comportamento
+            super(MyHandley, self).do_POST()
+
+enderoco_ip = "0.0.0.0"
+
+# define a porta a ser utilizada
+porta = 8000
+
+
+# cria um servidor na porta especificada
+with socketserver.TCPServer((enderoco_ip, porta), MyHandley) as httpd:  # " "aceita qualquer requisicao de rede
+    print(f"Servidor iniciado na porta {porta}")
+    # mantem o servidor em execução
+    httpd.serve_forever()
